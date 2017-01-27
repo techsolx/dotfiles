7a745f804f3de8fe35ccefe6143d72cbc9aa1396
JohnM <john@techsolx.com>
2017-01-26 21:49:32 -0700

    Update tmux to work with OSX and Linux in one config

--- .make.sh
+++ .make.sh
@@ -1,57 +1,61 @@
 # .make.sh
- # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
- # cd dotfiles
- # chmod +x .make.sh
- ##########
+# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+# cd dotfiles
+# chmod +x .make.sh
+##########
 
- ########## Variables
+########## Variables
 
- dir=~/dotfiles                    # dotfiles directory
- tmuxdir=~/dotfiles/tmux              # tmux folder is nested
- bashdir=~/dotfiles/bash                     # bash folder is nested
- Xinitdir=~/dotfiles/xinit                     # bash folder is nested
- vimdir=~/dotfiles/vim               # vim directory careful to ln -s .vim first
- olddir=~/dotfiles_old             # old dotfiles backup directory
- vimfiles="vim vimrc"    # list of files/folders to symlink in homedir
- bashfiles="bashrc profile bash_aliases"    # list of files/folders to symlink in homedir
- tmuxfiles="tmux.conf"
- Xinit="Xresources xinitrc"		# adding Xresources for urxvt enabled systems
+dir=~/dotfiles                    # dotfiles directory
+tmuxdir=~/dotfiles/tmux              # tmux folder is nested
+bashdir=~/dotfiles/bash                     # bash folder is nested
+Xinitdir=~/dotfiles/xinit                     # bash folder is nested
+vimdir=~/dotfiles/vim               # vim directory careful to ln -s .vim first
+olddir=~/dotfiles_old             # old dotfiles backup directory
+vimfiles="vim vimrc"    # list of files/folders to symlink in homedir
+bashfiles="bashrc profile bash_aliases"    # list of files/folders to symlink in homedir
+tmuxfiles="tmux.conf tmux-osx.conf"
+Xinit="Xresources xinitrc"		# adding Xresources for urxvt enabled systems
 
- ##########
+##########
 
- # create dotfiles_old in homedir
- echo "Creating $olddir for backup of any existing dotfiles in ~"
- mkdir -p $olddir
- echo "...done"
+# create dotfiles_old in homedir
+echo "Creating $olddir for backup of any existing dotfiles in ~"
+mkdir -p $olddir
+echo "...done"
 
- # change to the dotfiles directory
- echo "Changing to the $dir directory"
- cd $dir
- echo "...done"
+# change to the dotfiles directory
+echo "Changing to the $dir directory"
+cd $dir
+echo "...done"
 
- # move any existing vimfiles in homedir to dotfiles_old directory, then create symlinks
- for file in $vimfiles; do
-     echo "Moving any existing dotfiles from ~ to $olddir"
-     mv ~/.$file ~/dotfiles_old/
-     echo "Creating symlink to $file in home directory."
-     ln -s $dir/$file ~/.$file
- done
-
- # move any existing Xinit in homedir to dotfiles_old directory, then create symlinks
- for file in $Xinit; do
-     echo "Moving any existing dotfiles from ~ to $olddir"
-     mv ~/.$file ~/dotfiles_old/
-     echo "Creating symlink to $file in home directory."
-     ln -s $Xinitdir/$file ~/.$file
+# move any existing vimfiles in homedir to dotfiles_old directory, then create symlinks
+for file in $vimfiles; do
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/.$file ~/dotfiles_old/
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/$file ~/.$file
 done
 
- # move any existing bashfiles in homedir to dotfiles_old directory, then create symlinks
- for file in $bashfiles; do
-     echo "Moving any existing dotfiles from ~ to $olddir"
-     mv ~/.$file ~/dotfiles_old/
-     echo "Creating symlink to $file in home directory."
-     ln -s $bashdir/$file ~/.$file
- done
- # finish up with tmux files
-     echo "Now linking in tmux."
-     ln -s $tmuxdir/$tmuxfiles ~/.$tmuxfiles
+# move any existing Xinit in homedir to dotfiles_old directory, then create symlinks
+for file in $Xinit; do
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/.$file ~/dotfiles_old/
+    echo "Creating symlink to $file in home directory."
+    ln -s $Xinitdir/$file ~/.$file
+done
+
+# move any existing bashfiles in homedir to dotfiles_old directory, then create symlinks
+for file in $bashfiles; do
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/.$file ~/dotfiles_old/
+    echo "Creating symlink to $file in home directory."
+    ln -s $bashdir/$file ~/.$file
+done
+# finish up with tmux files
+for file in $tmuxfiles; do
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/.$file ~/dotfiles_old/
+    echo "Creating symlink to $file in home directory."
+    ln -s $tmuxdir/$file ~/.$file
+done
