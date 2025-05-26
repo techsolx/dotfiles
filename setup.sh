fc5dfcf3de3dc03e8aba8232af781e059bd56714
John MacTavish <john@techsolx.com>
2025-05-25 20:07:32 -0400

    fix(): remove xinit stuff in setup,
        * not necessary for workstation

--- setup.sh
+++ setup.sh
@@ -12,14 +12,12 @@ bashdir=~/dotfiles/bash      # folders are nested
 gitdir=~/dotfiles/git
 tmuxdir=~/dotfiles/tmux
 vimdir=~/dotfiles/vim        # vim directory careful to ln -s .vim first
-Xinitdir=~/dotfiles/xinit
 olddir=~/dotfiles_old        # old dotfiles backup directory
 bashfiles="bashrc bash_profile bash_aliases inputrc"    # list of files/folders to symlink in homedir
 ctagsfile="ctags"
 gitfiles="gitconfig"
 tmuxfiles="tmux.conf tmux-osx.conf"
 vimfiles="vim vimrc"         # list of files/folders to symlink in homedir
-Xinit="Xresources xinitrc"		# adding Xresources for urxvt enabled systems
 
 ##########
 
@@ -40,14 +38,6 @@ for file in $vimfiles; do
     mv ~/.$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
-done
-
-# move any existing Xinit in homedir to dotfiles_old directory, then create symlinks
-for file in $Xinit; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $Xinitdir/$file ~/.$file
 done
 
 # move any existing bashfiles in homedir to dotfiles_old directory, then create symlinks
