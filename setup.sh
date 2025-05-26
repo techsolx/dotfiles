936bb2eaf01e1e56955ba827393f7d6a935baeea
John MacTavish <john@techsolx.com>
2025-05-25 20:02:35 -0400

    feat(): add setup.sh file to workstations,
        * refresh bashrc from main,
        * pull in changes in tmux and vundle submodules
        * remove python-mode, too big

--- setup.sh
+++ setup.sh
@@ -7,15 +7,19 @@
 
 ########## Variables
 
-dir=~/dotfiles                    # dotfiles directory
-tmuxdir=~/dotfiles/tmux              # tmux folder is nested
-bashdir=~/dotfiles/bash                     # bash folder is nested
-vimdir=~/dotfiles/vim               # vim directory careful to ln -s .vim first
-olddir=~/dotfiles_old             # old dotfiles backup directory
-vimfiles="vim vimrc"    # list of files/folders to symlink in homedir
+dir=~/dotfiles               # dotfiles directory
+bashdir=~/dotfiles/bash      # folders are nested
+gitdir=~/dotfiles/git
+tmuxdir=~/dotfiles/tmux
+vimdir=~/dotfiles/vim        # vim directory careful to ln -s .vim first
+Xinitdir=~/dotfiles/xinit
+olddir=~/dotfiles_old        # old dotfiles backup directory
 bashfiles="bashrc bash_profile bash_aliases inputrc"    # list of files/folders to symlink in homedir
-tmuxfiles="tmux.conf tmux-osx.conf"
 ctagsfile="ctags"
+gitfiles="gitconfig"
+tmuxfiles="tmux.conf tmux-osx.conf"
+vimfiles="vim vimrc"         # list of files/folders to symlink in homedir
+Xinit="Xresources xinitrc"		# adding Xresources for urxvt enabled systems
 
 ##########
 
@@ -29,12 +33,21 @@ echo "Changing to the $dir directory"
 cd $dir
 echo "...done"
 
+#ToDo: dry this up!
 # move any existing vimfiles in homedir to dotfiles_old directory, then create symlinks
 for file in $vimfiles; do
     echo "Moving any existing dotfiles from ~ to $olddir"
     mv ~/.$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
+done
+
+# move any existing Xinit in homedir to dotfiles_old directory, then create symlinks
+for file in $Xinit; do
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/.$file ~/dotfiles_old/
+    echo "Creating symlink to $file in home directory."
+    ln -s $Xinitdir/$file ~/.$file
 done
 
 # move any existing bashfiles in homedir to dotfiles_old directory, then create symlinks
@@ -51,6 +64,14 @@ for file in $tmuxfiles; do
     mv ~/.$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
     ln -s $tmuxdir/$file ~/.$file
+done
+
+# git files
+for file in $gitfiles; do
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/.$file ~/dotfiles_old/
+    echo "Creating symlink to $file in home directory."
+    ln -s $gitdir/$file ~/.$file
 done
 
 # Now do ctags the long way
